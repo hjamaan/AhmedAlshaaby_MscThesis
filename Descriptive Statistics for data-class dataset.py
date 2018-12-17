543145fc217edffed56cf835626c9358d20171f2
Alshaaby <45258923+Alshaaby@users.noreply.github.com>
2018-12-17 15:02:20 +0300

    Update Descriptive Statistics for data-class dataset.py

--- Descriptive Statistics for data-class dataset.py
+++ Descriptive Statistics for data-class dataset.py
@@ -1,5 +1,5 @@
 #summrize statistics using pandas describe() method 
 import pandas as pd
 import numpy as np
-df= pd.read_csv('C:\\Users\POWER\Desktop\evaluation_dataset\data-class.csv')
+df= pd.read_csv('E:\\New folder\Dropbox\Thesis\DataAnalysis\Datasets\ReadyDatasets\OS1_Data_Class.csv')
 df.describe()
