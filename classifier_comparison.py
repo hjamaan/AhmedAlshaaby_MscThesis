0e6966d04a6381edeca8a0539c6e6ad73519b6d3
Ahmed AL-Shaaby <45258923+Alshaaby@users.noreply.github.com>
2019-01-21 12:59:11 +0300

    Update classifier_comparison.py

--- classifier_comparison.py
+++ classifier_comparison.py
@@ -27,41 +27,31 @@ datasets = ['OS1_Data_Class.csv','OS1_God_Class.csv', 'OS1_Long_Method.csv', 'OS
         'OS2_Xerces_God_Class.csv','OS2_Xerces_Spaghetti_Code.csv','OS2_Xerces_Swiss_Army_Knife.csv']
 
 
-# prepare models
+# Defined  models
 #--------------------------------------------
 models = []
+models.append(('RF', RandomForestClassifier()))
+models.append(('SVM', SVC()))
+models.append(('CART', DecisionTreeClassifier()))
 models.append(('LR', LogisticRegression()))
 models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
 models.append(('MLP', MLPClassifier()))
 models.append(('SGDC', SGDClassifier()))
-models.append(('KNNN', KNeighborsClassifier()))
 models.append(('SVM_linear', SVC()))
 models.append(('SVM_GAMA', SVC()))
 models.append(('GaussianP', GaussianProcessClassifier()))
-models.append(('CART', DecisionTreeClassifier(max_depth=5)))
-models.append(('RF', RandomForestClassifier()))
-models.append(('MLPP', MLPClassifier(alpha=1)))
-models.append(('ADB', AdaBoostClassifier()))
 models.append(('Quadra', QuadraticDiscriminantAnalysis()))
 #---------------------------
 
-# Specify the N fold
+# Specify the K-fold Crossvalidation
 num_folds = 10
 seed = 7
 kfold = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
-#kfold = cross_val_score.KFold( n_folds=num_folds, random_state=seed)
 df_list = []
-
-# Specify the N fold
-num_folds = 10
-seed = 7
-kfold = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
-#kfold = cross_val_score.KFold( n_folds=num_folds, random_state=seed)
+#--------------------------------------------------------
+ # For each dataset  evaluate each classifier 
 for dataset in datasets:
-  # evaluate each model in turn
   names = []
   accuracyresults = []
   accuracy = []
