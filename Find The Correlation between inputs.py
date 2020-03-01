5feda4adacb539f1e6a574ba248ffe627cf14758
Ahmed AL-Shaaby <45258923+Alshaaby@users.noreply.github.com>
2020-03-01 13:50:07 +0300

    Delete Find The Correlation between inputs.py

--- Find The Correlation between inputs.py
+++
@@ -1,5 +0,0 @@
-#Find The Correlation between inputs
-import pandas as pd
-import numpy as np
-df= pd.read_csv('E:\\New folder\Dropbox\Thesis\DataAnalysis\Datasets\ReadyDatasets\OS1_Data_Class.csv')
-df.corr()
